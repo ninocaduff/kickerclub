9a4e217ef977eb42d0ffa3ebbe7035eb7bfe689e
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-25 13:52:48 +0200

    Create other.js

---
+++ other.js
@@ -0,0 +1,7 @@
+setTimeout(function(){
+    $("#loading").addClass("animated fadeOut");
+    setTimeout(function(){
+      $("#loading").removeClass("animated fadeOut");
+      $("#loading").css("display","none");
+    },800);
+},1450);
