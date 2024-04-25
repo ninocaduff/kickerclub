25e1563e8ad85bac66454c6a4984278ed3f3878e
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-25 13:49:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,11 +23,3 @@ if ((width >= 900)){
     }
 }
 };
-
-setTimeout(function(){
-    $("#loading").addClass("animated fadeOut");
-    setTimeout(function(){
-      $("#loading").removeClass("animated fadeOut");
-      $("#loading").css("display","none");
-    },800);
-},1450);
