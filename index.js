49dcb750caa4967612a945eabc0b255c3d21efa8
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-26 11:42:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,15 +11,3 @@ $(document).ready(function(){
      } 
     });
 });
-
-var width = $(window).width(); 
-
-window.onscroll = function(){
-if ((width >= 900)){
-    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-        $("#middle").css("background-size","150% auto");
-    }else{
-        $("#middle").css("background-size","120% auto");        
-    }
-}
-};
