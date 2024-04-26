8dba839209da8490a3d506e8ca0f06f438f1369d
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-26 14:20:10 +0200

    Update board.js

--- board.js
+++ board.js
@@ -6,7 +6,11 @@ function loadBoardMembers() {
     // Example member data
     const boardMembers = [
         { name: "Mark Akhmetov", role: "President", image: "path_to_image_of_member1.jpg", info: "Mark Akhmetov is the president of Kicker Club. He has been actively involved in promoting the sport for over a decade." },
-        // Add more members here
+        { name: "Moritz Reher", role: "Vice President", image: "path_to_image_of_member1.jpg" },
+        { name: "Nino Caduff", role: "Head of Events & IT", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nino.jpg" },
+        { name: "Philipp Weber", role: "Head of Partnerships", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_philipp.jpg" },
+        { name: "Alessandro Giordano", role: "Head of Legal & Merch", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_alessandro.jpg" },
+        { name: "Nils Nguyen", role: "Head of Finance", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nils.jpeg" },
     ];
     
     // Empty the container before adding new content
