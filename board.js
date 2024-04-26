10789053c055b2a53746e80fea2c163685dd25dd
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-26 14:31:05 +0200

    Update board.js

--- board.js
+++ board.js
@@ -6,11 +6,11 @@ function loadBoardMembers() {
     // Example member data
     const boardMembers = [
         { name: "Mark Akhmetov", role: "President", image: "path_to_image_of_member1.jpg", info: "BWL" },
-        { name: "Moritz Reher", role: "Vice President", image: "path_to_image_of_member1.jpg" },
+        { name: "Moritz Reher", role: "Vice President", image: "path_to_image_of_member1.jpg", info "BWL" },
         { name: "Nino Caduff", role: "Head of Events & IT", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nino.jpg", info: "VWL" },
-        { name: "Philipp Weber", role: "Head of Partnerships", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_philipp.jpg" },
-        { name: "Alessandro Giordano", role: "Head of Legal & Merch", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_alessandro.jpg" },
-        { name: "Nils Nguyen", role: "Head of Finance", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nils.jpeg" },
+        { name: "Philipp Weber", role: "Head of Partnerships", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_philipp.jpg", info: "BWL" },
+        { name: "Alessandro Giordano", role: "Head of Legal & Merch", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_alessandro.jpg", info: "BLE" },
+        { name: "Nils Nguyen", role: "Head of Finance", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nils.jpeg", info: "BWL" },
     ];
     
     // Empty the container before adding new content
