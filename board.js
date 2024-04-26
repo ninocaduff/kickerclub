0f25472104ca270150e9cab763b30a2cf6316e15
Nino Caduff <150455581+ninocaduff@users.noreply.github.com>
2024-04-26 14:52:33 +0200

    Update board.js

--- board.js
+++ board.js
@@ -3,20 +3,17 @@ $(document).ready(function() {
 });
 
 function loadBoardMembers() {
-    // Example member data
     const boardMembers = [
-        { name: "Mark Akhmetov", role: "President", image: "path_to_image_of_member1.jpg", info: "BWL" },
-        { name: "Moritz Reher", role: "Vice President", image: "path_to_image_of_member1.jpg", info: "BWL" },
+        { name: "Mark Akhmetov", role: "President", image: "actual_path_to_image_of_member1.jpg", info: "BWL" },
+        { name: "Moritz Reher", role: "Vice President", image: "actual_path_to_image_of_member2.jpg", info: "BWL" },
         { name: "Nino Caduff", role: "Head of Events & IT", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nino.jpg", info: "VWL" },
         { name: "Philipp Weber", role: "Head of Partnerships", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_philipp.jpg", info: "BWL" },
         { name: "Alessandro Giordano", role: "Head of Legal & Merch", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_alessandro.jpg", info: "BLE" },
         { name: "Nils Nguyen", role: "Head of Finance", image: "https://raw.githubusercontent.com/ninocaduff/kickerclub/master/board/board_photo_nils.jpeg", info: "BWL" },
     ];
-    
-    // Empty the container before adding new content
+
     $('#board').empty();
 
-    // Dynamically generate and append member information to the board container
     boardMembers.forEach(member => {
         $('#board').append(
             `<div class="member">
